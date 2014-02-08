da7612e558431addbca8f73ad1a5f679687b6521
Yifan Gu <guyifan1121@gmail.com>
2014-02-08 17:08:57 -0500

    add some doc

--- recentf-wrapper.el
+++ recentf-wrapper.el
@@ -12,11 +12,21 @@
 ;; put this file in your emacs load path.
 ;; add following lines in your .emacs file:
 ;;
+;; 1, enable recentf:
+;;
+;; (require 'recentf)
+;; (recentf-mode 1)
+;; (setq recentf-max-menu-items 25)
+;; (global-set-key "\C-x\ \C-r" 'recentf-open-files)
+;;
+;; 2, enable wrapper:
+;;
 ;; (load "recentf-wrapper")
 ;; (require 'recentf_wrapper)
 ;; (global-set-key (kbd "C-x 4 C-r") 'open-recentf-in-new-window)
 ;;
 
+(require 'recentf)
 (require 'windmove)
 
 (defun open-recentf-in-new-window ()
